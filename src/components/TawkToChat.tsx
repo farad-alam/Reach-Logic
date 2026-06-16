3ae2967877b3767420257ccd83052734f5ec9a9f
Farad Alam <farad.dev@gmail.com>
2026-06-16 11:16:28 +0600

    fix: force Tawk.to chat widget to minimize on load

--- src/components/TawkToChat.tsx
+++ src/components/TawkToChat.tsx
@@ -25,6 +25,9 @@ export default function TawkToChat() {
       dangerouslySetInnerHTML={{
         __html: `
           var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          Tawk_API.onLoad = function() {
+            Tawk_API.minimize();
+          };
           (function(){
           var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
           s1.async=true;
